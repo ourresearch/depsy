a531d09701c73698d900a5496f5a69790bc8aef8
Heather Piwowar <hpiwowar@gmail.com>
2015-08-23 21:36:08 -0700

    main.py runs with positional arguments

--- main.py
+++ main.py
@@ -5,17 +5,27 @@ import argparse
 import logging
 
 
+def test_no_args():
+    print "test_no_args function ran"
 
-def test():
-    print "test function ran"
+def test_one_arg(one):
+    print "test_one_arg function ran", one
 
-def main(fn, args):
+def test_one_optional_arg(one=None):
+    print "test_one_optional_arg function ran", one
+
+def test_two_args(one, two=None):
+    print "test_two_args function ran", one, two
+
+
+def main(fn, optional_args=None):
+
     # call function by its name in this module, with all args :)
     # http://stackoverflow.com/a/4605/596939
-    if args:
-        globals()[fn](args)
+    if optional_args:
+        globals()[fn](*optional_args)
     else:
-        globals()[fn](**args.split(","))
+        globals()[fn]()
 
 
 if __name__ == "__main__":
@@ -25,19 +35,22 @@ if __name__ == "__main__":
     # get args from the command line:
     parser = argparse.ArgumentParser(description="Run stuff.")
     parser.add_argument('function', type=str, help="what function you want to run")
-    parser.add_argument('--args', default=None, type=str, help="optional args to pass to function")
+    parser.add_argument('optional_args', nargs='*', help="positional args for the function")
 
     args = vars(parser.parse_args())
-    function = args["function"]
 
-    arg_string = dict((k, v) for (k, v) in args.iteritems() if v and k!="function")
-    print u"main.py {function} with {arg_string}".format(
-        function=function.upper(), arg_string=arg_string)
+    function = args["function"]
+    optional_args = args["optional_args"]
+
+    print u"main.py {function} with {optional_args}".format(
+        function=function.upper(), optional_args=optional_args)
 
     global logger
-    logger = logging.getLogger("ti.daily.{function}".format(
+    logger = logging.getLogger("ti.main.{function}".format(
         function=function))
 
-    main(function, args)
+    main(function, optional_args)
 
     db.session.remove()
+
+
