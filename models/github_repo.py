bad068dee85545ab8828328201b88494f074cb93
Heather Piwowar <hpiwowar@gmail.com>
2015-09-01 10:58:02 -0700

    use rq for set_all_pypi_dependencies

--- models/github_repo.py
+++ models/github_repo.py
@@ -333,12 +333,19 @@ def set_all_pypi_dependencies(q_limit=100):
     q = q.order_by(GithubRepo.login)
     q = q.limit(q_limit)
 
-    # replace the RQ way for now, so it can run in parallel with another RQ task easily
-    for row in q.all():
-        #print "setting this row", row
-        set_pypi_dependencies(row[0], row[1])
+    # for row in q.all():
+    #     #print "setting this row", row
+    #     set_pypi_dependencies(row[0], row[1])
 
-    # return enque_repos(q, set_pypi_dependencies)
+    return enque_repos(q, set_pypi_dependencies)
+
+
+
+
+
+
+
+
 
 
 
