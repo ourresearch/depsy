afd74f284fdf6e745b8ba593ba5c4ef175a16b36
Heather Piwowar <hpiwowar@gmail.com>
2015-09-09 12:52:34 -0700

    try an rq worker that doesn't fork

--- rq_worker.py
+++ rq_worker.py
@@ -10,6 +10,103 @@ from app import redis_rq_conn
 from app import ti_queues
 import argparse
 
+
+# from https://github.com/michaelbrooks/rq-win/blob/master/rq_win/worker.py
+import time
+import sys
+import random
+
+import times
+import rq
+import rq.job
+import rq.compat
+import rq.worker
+class WindowsWorker(rq.Worker):
+    """
+    An extension of the RQ worker class
+    that works on Windows.
+    Does not support task timeouts
+    and will probably crash if the task goes badly,
+    due to not using fork().
+    """
+
+    def work(self, burst=False):
+        """Starts the work loop.
+        Pops and performs all jobs on the current list of queues.  When all
+        queues are empty, block and wait for new jobs to arrive on any of the
+        queues, unless `burst` mode is enabled.
+        The return value indicates whether any jobs were processed.
+        """
+        self.log.info('Using rq_win.WindowsWorker (experimental)')
+        return super(WindowsWorker, self).work(burst)
+
+
+    def fork_and_perform_job(self, job):
+        """Spawns a work horse to perform the actual work and passes it a job.
+        The worker will wait for the work horse and make sure it executes
+        within the given timeout bounds, or will end the work horse with
+        SIGALRM.
+        """
+        self.main_work_horse(job)
+
+    def main_work_horse(self, job):
+        """This is the entry point of the newly spawned work horse."""
+        # After fork()'ing, always assure we are generating random sequences
+        # that are different from the worker.
+        random.seed()
+
+        self._is_horse = True
+
+        success = self.perform_job(job)
+
+        self._is_horse = False
+
+    def perform_job(self, job):
+        """Performs the actual work of a job.  Will/should only be called
+        inside the work horse's process.
+        """
+        self.procline('Processing %s from %s since %s' % (
+            job.func_name,
+            job.origin, time.time()))
+
+        # try:
+        # I have DISABLED the time limit!
+        rv = job.perform()
+
+        # Pickle the result in the same try-except block since we need to
+        # use the same exc handling when pickling fails
+        job._result = rv
+        job._status = rq.job.JobStatus.FINISHED
+        job.ended_at = times.now()
+
+        result_ttl = job.get_ttl(self.default_result_ttl)
+        pipeline = self.connection._pipeline()
+        if result_ttl != 0:
+            job.save(pipeline=pipeline)
+        job.cleanup(result_ttl, pipeline=pipeline)
+        pipeline.execute()
+
+        # # except:
+        #     # Use the public setter here, to immediately update Redis
+        #     job.status = rq.job.JobStatus.FAILED
+        #     self.handle_exception(job, *sys.exc_info())
+        #     return False
+
+        if rv is None:
+            self.log.info('Job OK')
+        else:
+            self.log.info('Job OK, result = %s' % (rq.worker.yellow(rq.compat.text_type(rv)),))
+
+        if result_ttl == 0:
+            self.log.info('Result discarded immediately.')
+        elif result_ttl > 0:
+            self.log.info('Result is kept for %d seconds.' % result_ttl)
+        else:
+            self.log.warning('Result will never expire, clean up result key manually.')
+
+        return True
+
+
 def failed_job_handler(job, exc_type, exc_value, traceback):
 
     print "RQ job failed! {}. here's more: {} {} {}".format(
@@ -21,7 +118,7 @@ def start_worker(queue_name):
     print "starting worker '{}'...".format(queue_name)
 
     with Connection(redis_rq_conn):
-        worker = Worker(Queue(queue_name), exc_handler=failed_job_handler)
+        worker = WindowsWorker(Queue(queue_name), exc_handler=failed_job_handler)
         worker.work()
 
 
