7b0aa9e3e2e9b7936a9ae32bdcc0f35a40f90c82
Jason Priem <jp@jasonpriem.org>
2015-09-13 23:54:47 -0700

    save point for bed.

--- models/tag.py
+++ models/tag.py
@@ -3,20 +3,12 @@ from sqlalchemy.dialects.postgresql import JSONB
 
 class Tag(db.Model):
     __tablename__ = 'tags'
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.Text)
-    name = db.Column(db.Text)
-    other_names = db.Column(JSONB)
-    github_login = db.Column(db.Text)
-    github_about = db.deferred(db.Column(JSONB))
-    bucket = db.Column(JSONB)
-    sort_score = db.Column(db.Float)
+    id = db.Column(db.text, primary_key=True)
+    tag = db.Column(db.Text)
+    namespace = db.Column(db.Text)
+    count = db.Column(db.Integer)
 
-    type = db.Column(db.Text)
 
 
     def __repr__(self):
-        return u'<Person names "{name}" ({id})>'.format(
-            name=self.name,
-            id=self.id
-        )
+        return u'<Tag "{}">'.format(self.id)
