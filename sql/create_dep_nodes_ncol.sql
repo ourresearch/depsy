4a5b6fe740961ec1c2589b7ed1240dff2e591bd2
Heather Piwowar <hpiwowar@gmail.com>
2015-09-18 13:15:56 -0700

    r python rev deps

--- sql/create_dep_nodes_ncol.sql
+++ sql/create_dep_nodes_ncol.sql
@@ -1,6 +1,22 @@
-create table dep_nodes_ncol as
+create table dep_nodes_ncol_cran as
 (
-select jsonb_array_elements_text(named_deps) as package, 'github:'||id as used_by,  1.0 + ((api_raw->>'stargazers_count')::float / 10.0) as weight
+select jsonb_array_elements_text(named_deps) as package, 'github:'||id as used_by
+    from github_repo 
+    where named_deps is not NULL 
+    and language='r'
+    and api_raw->>'fork' = 'false'
+    and id not in
+        (select github_owner||':'||github_repo_name from package where host='cran' and github_owner is not null)
+union    
+select jsonb_array_elements_text(host_reverse_deps) as package, project_name as used_by
+    from package where host_reverse_deps is not NULL 
+    and host='cran' 
+) 
+
+
+create table dep_nodes_ncol_pypi as
+(
+select jsonb_array_elements_text(named_deps) as package, 'github:'||id as used_by
     from github_repo 
     where named_deps is not NULL 
     and language='python'
@@ -8,9 +24,16 @@ select jsonb_array_elements_text(named_deps) as package, 'github:'||id as used_b
     and id not in
         (select github_owner||':'||github_repo_name from package where host='pypi' and github_owner is not null)
 union    
-select jsonb_array_elements_text(host_reverse_deps) as package, project_name as used_by,  1.0 as weight
+select jsonb_array_elements_text(host_reverse_deps) as package, project_name as used_by
     from package where host_reverse_deps is not NULL 
     and host='pypi' 
 ) 
 
-select * from dep_nodes_ncol limit 1000
+
+select * from dep_nodes_ncol_pypi limit 1000
+
+{"reverse_depends": [], 
+"reverse_imports": [], 
+"all_reverse_deps": ["htmlTable"], 
+"reverse_enhances": [], 
+"reverse_suggests": ["htmlTable"]}
