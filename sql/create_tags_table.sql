c1b0c826965125b85b2d12745698559125327b95
Heather Piwowar <hpiwowar@gmail.com>
2015-09-25 09:00:41 -0700

    create tags table update

--- sql/create_tags_table.sql
+++ sql/create_tags_table.sql
@@ -1,12 +1,12 @@
 create table tags as
 select jsonb_array_elements_text(tags)::text as unique_tag, 
-	count(jsonb_array_elements_text(tags)::text) as count
+	count(jsonb_array_elements_text(tags)::text) as count,
+	host as namespace
  	from package 
- 	group by unique_tag
+ 	group by unique_tag, host
  	order by count desc
  	
-alter table tags add column namespace text
-update tags set namespace='pypi'
-
+alter table tags add column id text
 update tags set id=(namespace||':'||unique_tag)
+	
 select * from tags
