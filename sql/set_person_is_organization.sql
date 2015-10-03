d060061ad014fb37b017970c85fde7e40b880e80
Heather Piwowar <hpiwowar@gmail.com>
2015-10-03 02:14:59 -0700

    more orgs

--- sql/set_person_is_organization.sql
+++ sql/set_person_is_organization.sql
@@ -16,6 +16,7 @@ update person set is_organization=true
 	or name ilike '%project%'
 	or name ilike '%group%'
 	or name ilike '%alliance%'
+	or name ilike '%solutions%'
 	or name ilike '%contributor%'
 	or name ilike '%university%'
 	or name ilike '%department%'
@@ -27,4 +28,5 @@ update person set is_organization=true
 	or name like '% AG'
 	or name ilike '% GmbH'
 	or github_about->>'type' = 'Organization'
-	or name in ('Tryton', 'OpenERP SA')
+	or name in ('Tryton', 'OpenERP SA', 'OpenStack', 'Logilab')
+
