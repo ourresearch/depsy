967af9c3dc7defc62a98c4ed8b37b7a08c71efc8
Heather Piwowar <hpiwowar@gmail.com>
2015-10-09 08:25:19 -0700

    typo

--- scripts/run_igraph.sh
+++ scripts/run_igraph.sh
@@ -2,7 +2,7 @@
 # heroku run --size=performance-l sh scripts/run_igraph.sh
 
 echo "exporting CRAN dep nodes from db"
-psql $DATABASE_URL --command="\copy dep_nodes_ncol_cran_reverse to 'dep_enodes_ncol.txt' DELIMITER ' ';"
+psql $DATABASE_URL --command="\copy dep_nodes_ncol_cran_reverse to 'dep_nodes_ncol.txt' DELIMITER ' ';"
 echo "running CRAN igraph data through igraph and storing stats in db"
 python update.py CranPackage.set_igraph_data --limit=100000 --chunk=100 --no-rq
 
