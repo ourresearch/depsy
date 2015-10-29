69ceeceb09d91543d1d878c7b205e2c36f6f616c
Jason Priem <jp@jasonpriem.org>
2015-10-28 17:53:14 -0700

    try StringIO for badge instead of io.BytesIO

--- models/entity.py
+++ models/entity.py
@@ -1,5 +1,5 @@
 import requests
-import io
+from StringIO import StringIO
 from util import ordinal
 """
 Code shared among Person, Package, and Tag
@@ -25,6 +25,6 @@ def make_badge_io(entity):
         ret = '<svg xmlns="http://www.w3.org/2000/svg" width="260" height="20"><linearGradient id="b" x2="0" y2="100%"><stop offset="0" stop-color="#bbb" stop-opacity=".1"/><stop offset="1" stop-opacity=".1"/></linearGradient><mask id="a"><rect width="260" height="20" rx="3" fill="#fff"/></mask><g mask="url(#a)"><path fill="#555" d="M0 0h158v20H0z"/><path fill="#4c1" d="M158 0h102v20H158z"/><path fill="url(#b)" d="M0 0h260v20H0z"/></g><g fill="#fff" text-anchor="middle" font-family="DejaVu Sans,Verdana,Geneva,sans-serif" font-size="11"><text x="79" y="15" fill="#010101" fill-opacity=".3">Research software impact</text><text x="79" y="14">Research software impact</text><text x="208" y="15" fill="#010101" fill-opacity=".3">42nd percentile</text><text x="208" y="14">42nd percentile</text></g></svg>'
 
 
-    return io.BytesIO(ret)
+    return StringIO(ret)
 
 
