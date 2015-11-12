21069ff617e34454f717ff67830f28515a0adad7
Heather Piwowar <hpiwowar@gmail.com>
2015-11-12 10:11:26 -0800

    more special cases

--- scripts/update_dedup_people.sh
+++ scripts/update_dedup_people.sh
@@ -1,4 +1,5 @@
 heroku run --size=performance-l python update.py Package.dedup_people --no-rq --limit=1000000 --chunk=100
+heroku run --size=performance-l python update.py Package.dedup_special_cases --no-rq --limit=1000000 --chunk=100
 heroku run --size=performance-l python update.py Package.set_credit --no-rq --limit=1000000 --chunk=100
 
 heroku run --size=performance-l python update.py Person.set_scores --no-rq --limit=1000000 --chunk=100
